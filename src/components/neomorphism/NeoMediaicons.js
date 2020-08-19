aa19f203b683b69c1b128d53e27066d30c061f61
axel <axel-bentancur@hotmail.com>
2020-08-19 17:25:27 -0300

    Neomorphism Media Icons Design added

---
+++ src/components/neomorphism/NeoMediaicons.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import "./NeoMediaicons.css";
+
+export default class NeoMediaicons extends Component {
+  render() {
+    return (
+      <div className="box">
+        <div className="container">
+          <h1>NEOMORPHISM SOCIAL MEDIA ICONS</h1>
+          <div className="social-media">
+            <a href="asd">
+              <i className="fab fa-facebook"></i>
+            </a>
+            <a href="asd">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a href="asd">
+              <i className="fab fa-instagram"></i>
+            </a>
+            <a href="asd">
+              <i className="fab fa-whatsapp"></i>
+            </a>
+            <a href="asd">
+              <i className="fab fa-youtube"></i>
+            </a>
+            <a href="asd">
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="asd">
+              <i className="fab fa-github"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
