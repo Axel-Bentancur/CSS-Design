149b1d615682b28e2f42f2951847437b182c5afa
axel <axel-bentancur@hotmail.com>
2020-09-05 18:26:57 -0300

    skill portfolio first try

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
-import NeoMediaicons from "./components/neomorphism/NeoMediaicons";
+/* import NeoMediaicons from "./components/neomorphism/NeoMediaicons";
 import NeoLoginForm from "./components/neomorphism/NeoLoginForm";
-import NeoCheckButton from "./components/neomorphism/NeoCheckButton";
+import NeoCheckButton from "./components/neomorphism/NeoCheckButton"; */
+import NeoSkillButton from "./components/neomorphism/NeoSkillButton";
 
 function App() {
   return (
     <div className="container">
-      <NeoMediaicons />
+      {/*       <NeoMediaicons />
       <NeoLoginForm />
-      <NeoCheckButton />
+      <NeoCheckButton /> */}
+      <NeoSkillButton />
     </div>
   );
 }
---
+++ src/components/neomorphism/NeoSkillButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./NeoSkillButton.css";
+
+export default function NeoSkillButton() {
+  return (
+    <div>
+      <div className="skill-section">
+        <div className="skill-bg">
+          <span>Skills</span>
+        </div>
+      </div>
+    </div>
+  );
+}
